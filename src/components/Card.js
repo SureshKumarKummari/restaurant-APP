19b39b93ed98813bbea176e27d9b315890beb234
unknown <sureshk28591@gmail.com>
2025-01-05 12:25:49 +0530

    Third commit

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import "./Card.css";
+
+
+export default function Card(){
+    return(
+        <div className="card">
+            <h1>Delicious Food, Delivered to You</h1>
+            <p>Choose favorite meal from our broad selection of available meals and enjoy a delicious</p>
+            <p>lunch or dinner at home</p>
+            <p>All our meals are cooked with high-quality ingredients, just-in time and of course by </p>
+            <p>experinced chefs!</p>
+        </div>
+    );
+}
